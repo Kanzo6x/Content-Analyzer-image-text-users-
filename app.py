a078c4ed4b1e4593cf044c204bcba303c0308367
Amr Naser <wael01148799458@gmail.com>
2025-06-12 21:42:54 +0300

    changing the port

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ api.add_resource(ImageAiModelResource, '/checkimage', endpoint='checkimage')
 api.add_resource(RecommendationResource, '/recommend', endpoint='recommend')
 
 def main():
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0',port=5000)
 
 if __name__ == '__main__':
     main()
